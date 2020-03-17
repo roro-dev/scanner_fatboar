5dfcb54ba10772207fa13d3e3ed92cfae20f49af
Stef Rod <stefanedr.dev@gmail.com>
2020-03-17 15:51:49 +0100

    scanner added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:barcode_scan/barcode_scan.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
@@ -29,8 +33,44 @@ class _MyScannerState extends State<MyScanner>{
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color(0xffE4993D),
-        title: Text('Scanner Ticket'),
+        centerTitle: true,
+        title: Text('Scanner'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget> [
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+              child: RaisedButton(
+                color: Colors.blue,
+                textColor: Colors.white,
+                splashColor: Colors.blueGrey,
+                onPressed: scan,
+                child: const Text('SCAN QR CODE')
+              ),
+            ),
+          ]
+        )
       ),
     );
+  }
+}
+
+Future scan() async {
+  try {
+    String barcode = await BarcodeScanner.scan();
+    print(barcode);
+  } on PlatformException catch (e) {
+    if (e.code == BarcodeScanner.CameraAccessDenied) {
+      print('The user did not grant the camera permission!');
+    } else {
+      print('Unknown error: $e');
+    }
+  } on FormatException{
+    print('null (User returned using the "back"-button before scanning anything. Result)');
+  } catch (e) {
+    print('Unknown error: $e');
   }
 }
